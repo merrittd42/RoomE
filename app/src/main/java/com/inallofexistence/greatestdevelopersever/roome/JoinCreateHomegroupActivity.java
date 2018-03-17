b650012db25d52efa481af285effe6265ed4d263
Dillon Merritt <merrittd42@gmail.com>
2018-03-17 17:40:24 -0400

    Added in homegroup setup layout.

--- app/src/main/java/com/inallofexistence/greatestdevelopersever/roome/JoinCreateHomegroupActivity.java
+++ app/src/main/java/com/inallofexistence/greatestdevelopersever/roome/JoinCreateHomegroupActivity.java
@@ -2,6 +2,10 @@ package com.inallofexistence.greatestdevelopersever.roome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class JoinCreateHomegroupActivity extends AppCompatActivity {
 
@@ -9,5 +13,8 @@ public class JoinCreateHomegroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_create_homegroup);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        Log.d("HEY", user.getDisplayName() );
     }
 }
